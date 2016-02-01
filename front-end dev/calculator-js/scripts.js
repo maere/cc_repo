da8af46f4ecf896ef0142eaecdeb0917215ce7ec
maere <mary.sweeney@gmail.com>
2016-02-01 15:39:42 -0800

    wired up buttons to click events

---
+++ front-end dev/calculator-js/scripts.js
@@ -0,0 +1,80 @@
+//global variables
+var entry;
+
+//19 on click events, one for each button :/
+//alt option is to iterate through class array each time and check
+$('#clear').click(function(){
+   entry = $(this).text();
+});
+
+$('#ce').click(function(){
+   entry = $(this).text();
+});
+
+$('#percent').click(function(){
+   entry = $(this).text();
+});
+
+$('#divide').click(function(){
+   entry = $(this).text();
+});
+
+$('#seven').click(function(){
+   entry = $(this).text();
+});
+
+$('#eight').click(function(){
+   entry = $(this).text();
+});
+
+$('#nine').click(function(){
+   entry = $(this).text();
+});
+
+$('#multiply').click(function(){
+   entry = $(this).text();
+});
+
+$('#four').click(function(){
+   entry = $(this).text();
+});
+
+$('#five').click(function(){
+   entry = $(this).text();
+});
+
+$('#six').click(function(){
+   entry = $(this).text();
+});
+
+$('#subtract').click(function(){
+   entry = $(this).text();
+});
+
+$('#one').click(function(){
+   entry = $(this).text();
+});
+
+$('#two').click(function(){
+   entry = $(this).text();
+});
+
+$('#three').click(function(){
+   entry = $(this).text();
+});
+
+$('#add').click(function(){
+   entry = $(this).text();
+});
+
+$('#zero').click(function(){
+   entry = $(this).text();
+});
+
+$('#period').click(function(){
+   entry = $(this).text();
+});
+
+$('#equals').click(function(){
+   entry = $(this).text();
+});
