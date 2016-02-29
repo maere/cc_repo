147e37f92cf18a4ad31afcb9b5088226e8e9237d
maere <mary.sweeney@gmail.com>
2016-02-29 11:14:06 -0800

    front end layout for weather app

---
+++ front-end dev/weather/weather.js
@@ -0,0 +1,16 @@
+//when page HTML is loaded, these function can run
+
+//need function to check current weather
+//once data is retrieved, the page gets filled in (or swapped?) with:
+
+  //depending on conditions loads background
+      // and/or icon for general conditions (sunny, cloudy, partly cloudy, rain, drizzle, snow, sleet, etc.)
+  //location name according to lat & long?
+  //temperature
+  //temperature switcher button // need C/F converter function
+  //description (conditions): eg partly cloudy
+  //wind speeed (and dir)
+
+
+
+//apiKey = "8a8400a86a680c8a6a162acb0649285a"
