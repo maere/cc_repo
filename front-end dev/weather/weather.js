4af159c3271d9366d6a1f32d5317b068c4bc0ee0
maere <mary.sweeney@gmail.com>
2016-03-02 09:17:22 -0800

    working on F/C conversion piece

--- front-end dev/weather/weather.js
+++ front-end dev/weather/weather.js
@@ -2,6 +2,36 @@
    getLocation();
  });
 
+ $(document).on('click', 'fahrenheit', function(e){
+     $('button').removeClass('active');
+     $(this).addClass('active');
+     //call function;
+     $('#temp').text(toFahrenheit(currTemp));
+ });
+
+ $(document).on('click', 'celsius', function(e){
+     $('button').removeClass('active');
+     $(this).addClass('active');
+     //call function;
+     $('#temp').text(toCelsius(currTemp));
+ });
+
+
+//  $("button").click(function () {
+//     $(this).toggleClass("active");
+//     //get id att value so you know which function to call
+//     convertTo = $(this).attr('id');
+//     //call temp function
+//     if(convertTo==="celsius"){
+//       calculatedTemp = toCelsius(currTemp);
+//     }else{
+//       calculatedTemp = toFahrenheit(currTemp);
+//     }
+//     //get selector and change data in box
+//     $('#temp').text(calculatedTemp);
+//
+// });
+
 //get user's location
 var currLatitude;
 var currLongitude;
@@ -9,8 +39,10 @@ var apiKey= "8a8400a86a680c8a6a162acb0649285a";
 
 var currLocation;
 var currTemp;
+var calculatedTemp;
 var currConditions;
 var currWind;
+var convertTo = "fahrenheit"
 
 function getLocation(){
   if(navigator.geolocation){
@@ -38,14 +70,18 @@ function getWeatherData(currLatitude, currLongitude){
       currWindDirection = json.wind.deg;
       //console.log(currLocation + ", " + currTemp + ", " + currConditions + ", " + currWind  + ", " + currWindDirection);
 
+      //call temperature calculation function, by default call Farenheit & have that button activited aleady in HTML
+      calculatedTemp = toFahrenheit(currTemp);
+
       $('.location').text(currLocation);
       //bg gets loaded depending on keyword in weather[0].main, will pull URL value from obj
-      $('#temp').text(currTemp);
+      $('#temp').text(Math.floor(calculatedTemp)); //can also use Math.round() for integer
       $('#conditions').text(currConditions + ", " + currDescription);
       $('#wind').text(currWindDirection + " degrees at " + currWind + " mph");
 
     });
   }
+
 
   //depending on conditions loads background (stored in an object {})
       // and/or icon for general conditions (sunny, cloudy, partly cloudy, rain, drizzle, snow, sleet, etc.)
@@ -58,10 +94,11 @@ function getWeatherData(currLatitude, currLongitude){
                     "tbd": "http://imagepathplaceholder"};
 
     //temperature switcher buttons // need C/F converter functiona
-    function toFahrenheit(){
-
+    //(K - 273.15) * 9/5 + 32
+    function toFahrenheit(tempKelvin){
+      return ((tempKelvin - 273.15)*1.8 + 32);
     }
-
-    function toCelsius(){
-
+    //K - 273.15
+    function toCelsius(tempKelvin){
+      return tempKelvin - 273.15;
     }
