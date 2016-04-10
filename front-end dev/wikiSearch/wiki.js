505539fd91f3742da68c54f2c77514223f857cdc
maere <mary.sweeney@gmail.com>
2016-04-09 18:23:29 -0700

    functioning - was not CORS issue but bootstrap form issue

--- front-end dev/wikiSearch/wiki.js
+++ front-end dev/wikiSearch/wiki.js
@@ -1,56 +1,52 @@
-//var searchString;
+
 var openSearchTest;
-var dataHolder = [];
-var titles;
-var descriptions;
-var links;
 
-$('#submitQuery').click(fetchString);
+$(document).on('click', '#submitQuery', function () {
 
-function fetchString(){
-  var searchString = $("#userQuery").val();
-  openSearchTest = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?" +   //jsoncallback=?
-      "action=opensearch&format=json&search=" + searchString + "&limit=10&suggest=1";
-      alert("Using this value for the call: " + openSearchTest);
-      loadData(openSearchTest);
-}
+  var searchString = $('#userQuery').val();
+  openSearchTest = "https://en.wikipedia.org/w/api.php?" +
+  "action=opensearch&format=json&search=" + searchString + "&limit=10&suggest=1&callback=?";
+  clearOldData();
+  loadData();
 
-function loadData(openSearchTest){
+  });
 
-    //alert("You got a search String: " + openSearchTest);
-    $.getJSON(openSearchTest, {
-      format: "json"
-    }, function(data){
-      //console.log(data);
-      titles = data[1];
-      descriptions = data[2];
-      links = data[3];
-      //console.log(titles, descriptions, links);
-      for(var i=0; i<titles.length; i++){
-          currObj = {
-            title: titles[i],
-            description: descriptions[i],
-            link: links[i]
+  //test
+  function loadData(){
+
+    // Using jQuery
+    $.ajax( {
+        url: openSearchTest,
+        dataType: 'jsonp',
+        timeout : 2000,
+        type: 'GET',
+        //headers: 'Access-Control-Allow-Origin: *',
+        success: function(data) {
+          //console.log(data);
+          titles = data[1];
+          descriptions = data[2];
+          links = data[3];
+
+          for(var i=0; i<titles.length; i++){
+
+            var data2 = {
+              title: titles[i],
+              description: descriptions[i],
+              link: links[i]
+            };
+
+            var output = $("#list");
+            var template = $("#template").html();
+            //var data2 = [title, description, link];
+            html = Mustache.render(template, data2);
+            output.append(html);
           }
-          var output = $("#list");
-          var template = $("#template").html();
-
-          html = Mustache.render(template, currObj);
-          output.append(html);
-        }                     //.done(function(dataHolder){})
-    }).fail(function(){
-          alert("There was an error with you request.");
-      });
-
+        }
+    });
   }
 
 
-
-// //maybe make titles, etc. global and move appending list into a separate function/callback
-  // function addDataToPage(){
-  //     var output = $("#list");
-  //       var template = $("#template").html();
-  //       //dataHolder = [title, description, link];
-  //       html = Mustache.render(template, dataHolder);
-  //       output.append(html);
-  // }
+  function clearOldData(){
+    $('#list').empty();
+    $('#userQuery').val("");
+  }
