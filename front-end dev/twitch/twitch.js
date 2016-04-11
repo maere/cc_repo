a321ec958be2b340cb529cf9efb7e1ef6c593aac
maere <mary.sweeney@gmail.com>
2016-04-11 14:24:20 -0700

    check if live and pull data

--- front-end dev/twitch/twitch.js
+++ front-end dev/twitch/twitch.js
@@ -7,13 +7,49 @@ var users = ["freecodecamp", "storbeck", "terakilobyte",
 $(document).ready(function(){
 
   //call w test data
-  getChannelData(users[0]);
+  getStreamStatus('cretetion');
+  //getChannelData(brunofin); //if returns undefined, then put in placeholder
 
 });
 
 
-function getChannelData(channelName){
+//find out weather the channel is streaming,
+//if yes, light up green and call function to get additional data and display it if no, display gray
+function getStreamStatus(channelName){
+  //https://api.twitch.tv/kraken/streams/freecodecamp'
 
+  $.ajax('https://api.twitch.tv/kraken/streams/' + channelName, {
+      jsonp: 'callback',
+      dataType: 'jsonp',
+      Accept: 'application/vnd.twitchtv.v3+json'
+  }).then(function(data) {
+    //console.log(data);
+      // handle requested data from server
+      if(data['stream']==null){
+        alert("Not streaming: " + data['stream']);
+      }
+      else{
+
+          var viewers = data['stream']['viewers'];
+          var game = data['stream']['game'];
+          var liveURL = data['stream']['channel']['url'];
+          var views = data['stream']['channel']['views'];
+          var followers = data['stream']['channel']['followers'];
+          var logo = data['stream']['channel']['logo'];
+          console.log("Viewers: " + viewers + ' Game: ' + game + ' Followers:  ' +  followers + ' Go Watch at: ' + liveURL);
+      }
+
+  });
+
+}
+
+
+
+
+
+
+//gets channel data -- doesn't matter if online - feeds div
+function getChannelData(channelName){
   $.ajax('https://api.twitch.tv/kraken/channels/' + channelName, {
       jsonp: 'callback',
       dataType: 'jsonp',
