65807beec2f1607cb9b4e429544e13058e09daaa
maere <mary.sweeney@gmail.com>
2016-04-16 15:20:45 -0700

    filling in basic data

--- front-end dev/twitch/twitch.js
+++ front-end dev/twitch/twitch.js
@@ -1,15 +1,14 @@
 var users = ["freecodecamp", "storbeck", "terakilobyte",
 "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
-
 $(document).ready(function(){
+  //creates and array to store the JSON object from the API call
+  getChannels(users, callback); //should have a callback, so that when the data has arrived it can process
 
-  //call w test data
   //getStreamStatus('cretetion');
   //getChannelData(brunofin); //if returns undefined, then put in placeholder
-  getChannelData(users[0]);
+  //getChannelData(users[0]);
 
 });
-
 
 //find out weather the channel is streaming,
 //if yes, light up green and call function to get additional data and display it if no, display gray
@@ -34,16 +33,8 @@ function getStreamStatus(channelName){
           var logo = data['stream']['channel']['logo'];
           console.log("Viewers: " + viewers + ' Game: ' + game + ' Followers:  ' +  followers + ' Go Watch at: ' + liveURL);
       }
-
   });
-
 }
-
-
-
-
-
-
 //gets channel data -- doesn't matter if online - feeds div
 function getChannelData(channelName){
   $.ajax('https://api.twitch.tv/kraken/channels/' + channelName, {
@@ -56,20 +47,25 @@ function getChannelData(channelName){
   }).then(function(response) {
     console.log(response);
       // handle requested data from server
-      var logo = response['logo']; 
+      var logo = response['logo'];
       var displayName = response['display_name'];
       var status = response['status'];
       var url = response['url'];
-
-        // console.log(displayName);
-        // console.log(status);
-        // console.log(url);
-
-      $('#output').append(document.createTextNode(displayName));
-      $('#output').append('<br>');
-      $('#output').append(document.createTextNode(status));
-      $('#output').append('<br>');
-      $('#output').append(document.createTextNode(url));
-
+// console.log(displayName);  // console.log(status);  // console.log(url); // console.log(logo);
+      //fills in basic channel data whether or not streaming
+        $('#title').text(displayName);
+        $('#stream-data a').attr('href', url);
+        $('#stream-data a').text(url);
+        $('#channel-logo img').attr('src', logo);
   });
+}
+
+
+//makes the API call for each channel and stores all returned objects in an array
+//has a callback that executes the next part once data is returned
+function getChannels(channelArray, callback){
+  
+
+
+  callback();
 }
