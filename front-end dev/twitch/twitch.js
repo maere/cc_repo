c5b409fe2486050ba5d26ddadd49c93c94506506
maere <mary.sweeney@gmail.com>
2016-04-21 15:08:50 -0700

    change to make the 2 calls in one function and return promises

--- front-end dev/twitch/twitch.js
+++ front-end dev/twitch/twitch.js
@@ -1,68 +1,45 @@
 var allChannelData = [];
 var placeholder_logo = "http://placehold.it/100x100";
 var placeholder_status = "Offline";
+var counter =0;
 
-var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+var tempChannelData;
+var tempStreamData;
+
+
+var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb",
+"thomasballinger","noobs2ninjas","beohoff", "immirath", "brunofin", "comster404"];
 
 $(document).ready(function(){
-  //creates and array to store the JSON object from the API call
-  getChannels(users); //should have a callback, so that when the data has arrived it can process
+  //call dual api call function
+  getChannelData("sodapoppin");
+
+});
+  // getChannels(users); //should have a callback, so that when the data has arrived it can process
+  // getAllStreams(users);
   //displayChannelData(allChannelData);
   //getStreamStatus('cretetion');
   //getChannelData(brunofin); //if returns undefined, then put in placeholder
   //getChannelData(users[0]);
+  //console.log(allChannelData);
 
-});
+  //then call this stuff:
 
-//find out weather the channel is streaming,
-//if yes, light up green and call function to get additional data and display it if no, display gray
-function getStreamStatus(channelName){
-  //https://api.twitch.tv/kraken/streams/freecodecamp'
+          // cleanData(currentChannel);
+          //
+          // displayChannelData(currentChannel); //this should be a call back that runs after data is all processed...takes objc
 
-  $.ajax('https://api.twitch.tv/kraken/streams/' + channelName, {
-      jsonp: 'callback',
-      dataType: 'jsonp',
-      Accept: 'application/vnd.twitchtv.v3+json'
-  }).then(function(data) {
 
-      if(data['stream']==null){
-        alert("Not streaming: " + data['stream']);
-      }
-      else{
-          var viewers = data['stream']['viewers'];
-          var game = data['stream']['game'];
-          var liveURL = data['stream']['channel']['url'];
-          var views = data['stream']['channel']['views'];
-          var followers = data['stream']['channel']['followers'];
-          var logo = data['stream']['channel']['logo'];
-          console.log("Viewers: " + viewers + ' Game: ' + game + ' Followers:  ' +  followers + ' Go Watch at: ' + liveURL);
-      }
-  });
-}
+
 //gets channel data -- doesn't matter if online - feeds div
-function getChannelData(channelName){
-  $.ajax('https://api.twitch.tv/kraken/channels/' + channelName, {
-      jsonp: 'callback',
-      dataType: 'jsonp',
-      Accept: 'application/vnd.twitchtv.v3+json'
-      // data: {
-      //     id: 123
-      // }
-  }).then(function(response) {
-    //console.log(response); //it gets this far.....
-      // handle requested data from server
-      var currentChannel = {
-        logo: response['logo'],
-        displayName: response['display_name'],
-        status: response['status'],
-        url: response['url']
-      }
-      cleanData(currentChannel);
-      //allChannelData.push(currentChannel);
-      displayChannelData(currentChannel);
-
-  });
-}
+// function getChannelData(channelName){
+//   var promise1 = $.ajax('https://api.twitch.tv/kraken/channels/' + channelName, {
+//       jsonp: 'callback',
+//       dataType: 'jsonp',
+//       Accept: 'application/vnd.twitchtv.v3+json'
+//   });
+//  return promise1;
+// }
 
 //helper function makes the API call for each channel and stores all returned objects in an array
 function getChannels(channelArray){
@@ -88,4 +65,99 @@ function displayChannelData(object){
                 //var data2 = [title, description, link];
     html = Mustache.render(template, object);
     output.append(html);
+}
+
+
+//find out weather the channel is streaming,
+//if yes, light up green and call function to get additional data and display it if no, display gray
+// function getStreamStatus(channelName){
+//   //https://api.twitch.tv/kraken/streams/freecodecamp'
+//  var apiCall = 'https://api.twitch.tv/kraken/streams/' + channelName;
+//
+//   var promise2 = $.ajax(apiCall, {
+//       jsonp: 'callback',
+//       dataType: 'jsonp',
+//       Accept: 'application/vnd.twitchtv.v3+json'
+//   });
+//   return promise2;
+// }
+
+function getAllStreams(userArray){
+  for(var i=0; i<userArray.length; i++){
+    getStreamStatus(userArray[i]);
+  }
+}
+
+function getChannelData(channelName){
+
+    var channelCall = $.get('https://api.twitch.tv/kraken/channels/' + channelName, {
+        jsonp: 'callback',
+        dataType: 'jsonp',
+        Accept: 'application/vnd.twitchtv.v3+json'
+    }).done(function( data ) {
+        tempChannelData = data;
+      }).error(function(){
+        alert("We caught this one as unprocessable--do not pass");
+        //need to set values here - this is a catch and set a boolean so it doesn't get processed by the next function
+      });
+    //console.log(channelCall);
+
+    var streamCall =  $.ajax('https://api.twitch.tv/kraken/streams/' + channelName, {
+          jsonp: 'callback',
+          dataType: 'jsonp',
+          Accept: 'application/vnd.twitchtv.v3+json'
+      }).done(function( data ) {
+
+          tempStreamData = data;
+        });
+      //console.log(streamCall);
+
+    $.when(channelCall, streamCall).then(function (response1, response2) {//can try to figure out how to pull it from here, or
+                                                  //continue to use the data holders from the done function
+      //to process data from channelCall
+    console.log(tempChannelData);
+    console.log(tempStreamData);
+
+      var currentChannel = {};
+
+      if(response1['status']=='422'){
+        currentChannel = {
+          logo: "",
+          displayName: tempChannelData['display_name'],
+          status: tempChannelData['message'],
+          url: ""
+        }
+      }else{
+          currentChannel = {
+            logo: tempChannelData['logo'],
+            displayName: tempChannelData['display_name'],
+            status: tempChannelData['status'],
+            url: tempChannelData['url']
+          }
+        }
+        //now we have an object called currentChannel with stuff in it, let's add the rest
+      if(tempStreamData['stream']===null){
+              alert('This channel is offline');
+            currentChannel['live'] = false;
+      } else{
+        currentChannel['live'] = true;
+        alert('This channel is streaming now');
+              //currentChannel['live'] = true;
+                //var idByURL = response2['_links']['self'];
+              // currentChannel['viewers'] = response2['stream']['viewers'];
+              // currentChannel['game'] = response2['stream']['game'];
+              // currentChannel['liveURL'] = response2['stream']['channel']['url'];
+              // currentChannel['views'] = response2['stream']['channel']['views'];
+              // currentChannel['followers'] = response2['stream']['channel']['followers'];
+              // currentChannel['logo'] = response2['stream']['channel']['logo'];
+            }
+
+            console.log(currentChannel);
+            allChannelData.push(currentChannel);
+            //console.log(allChannelData);
+});
+
+
+          //console.log(allChannelData);
+
 }
