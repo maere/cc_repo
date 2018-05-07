0ed0f967ad31a115a5aac8cd8823a17b43ad9658
maere <mary.sweeney@gmail.com>
2018-05-06 21:07:38 -0400

    https://www.freecodecamp.org/challenges/sum-all-odd-fibonacci-numberss

---
+++ intermediate algos/13_sum-all-odd-fibonacci.js
@@ -0,0 +1,26 @@
+function sumFibs(num) {
+
+  var fibArray = [];
+  fibArray.push(1);
+  fibArray.push(1);
+
+  if(num<=1){
+      return fibArray[num];
+   }
+
+  for(var i=2; i<=num; i++){
+    var currFibValue = fibArray[i-1] + fibArray[i-2];
+    fibArray[i] = currFibValue;
+  }
+
+  var summedNums = 0;
+  for(var j=0; fibArray[j]<=num; j++){
+    var currentVal = fibArray[j];
+    if(fibArray[j]%2!=0){
+      summedNums = summedNums + currentVal;
+    }
+  }
+  return summedNums;
+}
+
+sumFibs(4);
